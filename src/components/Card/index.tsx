0844a490b2225667ee99842de0a640c130792c99
Renan2411 <renanmd2411@gmail.com>
2023-04-04 21:45:22 -0400

    [COMMIT]- aula dia 04/04/2023 - estlizando card de dados de um usuário

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,18 +1,20 @@
-function Card(props) {
+import './styles.css'
+
+function Card({user}) {
     return (
         <div className='profile-card'>
             <div className="image">
                 <div className="profile-img">
-                    <span className="initials">{props.user.iniciais}</span>
+                    <span className="initials">{user.iniciais}</span>
                 </div>
             </div>
             <div className="text-data">
-                <span className="name">{props.user.nome}</span>
-                <span className="job">{props.user.profissao}</span>
+                <span className="name">{user.nome}</span>
+                <span className="job">{user.profissao}</span>
             </div>
             <div className="text-data">
-                <span className="cidade">{props.user.cidade}/{props.user.estado}</span>
-                <span className="aniversario">Aniversário: {props.user.nascimento}</span>
+                <span className="cidade">{user.cidade}/{user.estado}</span>
+                <span className="aniversario">Aniversário: {user.nascimento}</span>
             </div>
             <div className="media-buttons">
                 <a href="#" style={{ background: '#4267b2' }} className="link">
@@ -30,22 +32,22 @@ function Card(props) {
                 </a>
             </div>
             <div className="buttons">
-                <button className="button">Altura {props.user.altura}</button>
-                <button className="button">Peso {props.user.peso}kg</button>
-                <button className="button">IMC {props.user.imc}</button>
+                <button className="button">Altura {user.altura}</button>
+                <button className="button">Peso {user.peso}kg</button>
+                <button className="button">IMC {user.imc}</button>
             </div>
             <div className="analytics">
                 <div className="data">
                     <i className="bx bx-heart"></i>
-                    <span className="nu">{props.user.likes}k</span>
+                    <span className="nu">{user.likes}k</span>
                 </div>
                 <div className="data">
                     <i className="bx bx-message-rounded"></i>
-                    <span className="nu">{props.user.messages}k</span>
+                    <span className="nu">{user.messages}k</span>
                 </div>
                 <div className="data">
                     <i className="bx bx-share"></i>
-                    <span className="nu">{props.user.shares}k</span>
+                    <span className="nu">{user.shares}k</span>
                 </div>
             </div>
         </div>
